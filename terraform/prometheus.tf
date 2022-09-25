0bce16331e6ac2840bda1c86f256144575af97f2
Samuel Isola <dimeji.isola@gmail.com>
2022-09-26 00:01:04 +0200

    remove namespace

--- terraform/prometheus.tf
+++
@@ -1,6 +0,0 @@
-resource "helm_release" "prometheus" {
-  name       = "prometheus"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "prometheus-community"
-  namespace  = kubernetes_namespace_v1.monitoring.id
-}
--- terraform/shared.tf
+++ terraform/shared.tf
@@ -1,9 +1,1 @@
 data "google_client_config" "provider" {}
-
-resource "kubernetes_namespace_v1" "monitoring" {
-  metadata {
-    annotations = {
-      name = "monitoring"
-    }
-  }
-}
