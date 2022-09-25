ac7adfa046e90e78a58c773dcafc20d3da166f9a
Samuel Isola <dimeji.isola@gmail.com>
2022-09-25 22:42:47 +0200

    set k8s version to 1.24

--- terraform/shared.tf
+++ terraform/shared.tf
@@ -1,11 +1,5 @@
 data "google_client_config" "provider" {}
 
-resource "google_artifact_registry_repository" "registry" {
-  location      = var.region
-  repository_id = "tabeo-devops-chal"
-  format        = "DOCKER"
-}
-
 resource "kubernetes_namespace_v1" "monitoring" {
   metadata {
     annotations = {
